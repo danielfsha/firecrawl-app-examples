97bf06deeeadd8d5486bbd99ef986909ec48bcd9
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-07-08 01:22:37 +0300

    feat: add card component page

--- firecrawl-ui/.source/server.ts
+++ firecrawl-ui/.source/server.ts
@@ -1,8 +1,9 @@
 // @ts-nocheck
-import * as __fd_glob_5 from "../content/learn/installation.mdx?collection=learn"
-import * as __fd_glob_4 from "../content/components/tabs.mdx?collection=components"
-import * as __fd_glob_3 from "../content/components/switch.mdx?collection=components"
-import * as __fd_glob_2 from "../content/components/logo.mdx?collection=components"
+import * as __fd_glob_6 from "../content/learn/installation.mdx?collection=learn"
+import * as __fd_glob_5 from "../content/components/tabs.mdx?collection=components"
+import * as __fd_glob_4 from "../content/components/switch.mdx?collection=components"
+import * as __fd_glob_3 from "../content/components/logo.mdx?collection=components"
+import * as __fd_glob_2 from "../content/components/card.mdx?collection=components"
 import * as __fd_glob_1 from "../content/components/button.mdx?collection=components"
 import * as __fd_glob_0 from "../content/components/accordion.mdx?collection=components"
 import { server } from 'fumadocs-mdx/runtime/server';
@@ -13,8 +14,8 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const components = await create.docs("components", "content/components", {}, {"accordion.mdx": __fd_glob_0, "button.mdx": __fd_glob_1, "logo.mdx": __fd_glob_2, "switch.mdx": __fd_glob_3, "tabs.mdx": __fd_glob_4, });
+export const components = await create.docs("components", "content/components", {}, {"accordion.mdx": __fd_glob_0, "button.mdx": __fd_glob_1, "card.mdx": __fd_glob_2, "logo.mdx": __fd_glob_3, "switch.mdx": __fd_glob_4, "tabs.mdx": __fd_glob_5, });
 
 export const docs = await create.docs("docs", "content/docs", {}, {});
 
-export const learn = await create.docs("learn", "content/learn", {}, {"installation.mdx": __fd_glob_5, });
+export const learn = await create.docs("learn", "content/learn", {}, {"installation.mdx": __fd_glob_6, });
