9c31c60015307990071cab2373041dabed454926
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-07-08 00:33:48 +0300

    feat: add logo component page

--- firecrawl-ui/.source/server.ts
+++ firecrawl-ui/.source/server.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import * as __fd_glob_3 from "../content/components/tabs.mdx?collection=components"
-import * as __fd_glob_2 from "../content/components/switch.mdx?collection=components"
+import * as __fd_glob_4 from "../content/components/tabs.mdx?collection=components"
+import * as __fd_glob_3 from "../content/components/switch.mdx?collection=components"
+import * as __fd_glob_2 from "../content/components/logo.mdx?collection=components"
 import * as __fd_glob_1 from "../content/components/button.mdx?collection=components"
 import * as __fd_glob_0 from "../content/components/accordion.mdx?collection=components"
 import { server } from 'fumadocs-mdx/runtime/server';
@@ -11,6 +12,6 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const components = await create.docs("components", "content/components", {}, {"accordion.mdx": __fd_glob_0, "button.mdx": __fd_glob_1, "switch.mdx": __fd_glob_2, "tabs.mdx": __fd_glob_3, });
+export const components = await create.docs("components", "content/components", {}, {"accordion.mdx": __fd_glob_0, "button.mdx": __fd_glob_1, "logo.mdx": __fd_glob_2, "switch.mdx": __fd_glob_3, "tabs.mdx": __fd_glob_4, });
 
 export const docs = await create.docs("docs", "content/docs", {}, {});
